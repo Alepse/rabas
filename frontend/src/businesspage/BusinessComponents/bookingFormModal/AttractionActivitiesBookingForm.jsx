008e7ed5771248f9ed7447619995b350335fc8e7
Alepse <kenespela@gmail.com>
2024-11-09 10:44:56 +0800

    ui fixes

--- frontend/src/businesspage/BusinessComponents/bookingFormModal/AttractionActivitiesBookingForm.jsx
+++ frontend/src/businesspage/BusinessComponents/bookingFormModal/AttractionActivitiesBookingForm.jsx
@@ -267,7 +267,7 @@ const AttractionActivitiesBookingForm = ({ isOpen, onClose, product = {} }) => {
           <ModalBody className="space-y-6">
             {steps[currentStep]}
             <div className="flex justify-between mt-4">
-              <div>
+            <div className='flex flex-wrap gap-3'>
                 <Button auto flat color="danger" onClick={onClose} className="mr-2">
                   Cancel
                 </Button>
