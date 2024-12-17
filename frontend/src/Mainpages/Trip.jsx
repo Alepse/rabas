97aba6d854d6f008a0fb0d5fbee6b1d0ec09fedb
Alepse <kenespela@gmail.com>
2024-12-17 11:01:35 +0800

    fix responsiveness nung search.jsx

--- frontend/src/Mainpages/Trip.jsx
+++ frontend/src/Mainpages/Trip.jsx
@@ -257,16 +257,17 @@ const Trip = () => {
   };
 
   return (
-    <div className="mx-auto bg-gray-100 min-h-screen font-sans flex flex-col" style={{ backgroundImage: `url(${wave})`, backgroundSize: 'auto', backgroundRepeat: 'repeat', backgroundPosition: 'center' }}>
+    <div className="mx-auto  bg-gray-100 min-h-screen font-sans flex flex-col" style={{ backgroundImage: `url(${wave})`, backgroundSize: 'auto', backgroundRepeat: 'repeat', backgroundPosition: 'center' }}>
       <Nav />
       <div className="mt-[3rem] flex justify-center w-full px-4">
         <Search />
       </div>
-
-      <div className="container mx-auto flex justify-center bg-color1 p-4 mb-4 items-center shadow-lg md:rounded-xl shadow-color1 m-4">
+      <div className='p-6'>
+      <div className="container mx-auto flex justify-center bg-color1 p-4 mb-4 items-center shadow-lg rounded-xl shadow-color1 m-4">
         <h1 className="text-white text-center font-semibold text-2xl md:text-4xl hover:tracking-wider duration-500">
           Plan Your Trip In Sorsogon
         </h1>
+      </div>
       </div>
 
       <div className="container mx-auto p-4 md:p-9 bg-white max-h-screen mb-2 border rounded-lg shadow-md">
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -159,13 +159,13 @@ const Search = () => {
           onSelectionChange={handleTabChange}
           variant="underlined"
           classNames={{
-          base: "w-full overflow-x-auto rounded shadow-md",
-          tabList: "gap-6 flex justify-center w-full p-4 border-gray-200 container ",
-          tab: "max-w-fit px-0 h-12 ",
-          tabContent: " text-color1  ",
-          cursor: "w-full bg-color1",
-        
-        }}
+            base: "w-full overflow-x-auto rounded-full",
+            tabList: "gap-6 w-full p-4 flex md:justify-center",
+            tab: "max-w-fit px-0 h-12",
+            tabContent: "text-color1",
+            cursor: "w-full bg-color1",
+          }}
+          defaultValue="all"
         >
           <Tab key="all" title={<span className="flex items-center"><FaSearch className="mr-2" />Search All</span>} />
           <Tab key="activities" title={<span className="flex items-center"><FaHiking className="mr-2" />Activities</span>} />
