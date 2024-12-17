3dd5e9d57893ddf4caed125d5d9c9556de8d9fe0
Alepse <kenespela@gmail.com>
2024-12-17 21:17:28 +0800

    .

--- frontend/src/Mainpages/HomePageSections/DestinationSection.jsx
+++ frontend/src/Mainpages/HomePageSections/DestinationSection.jsx
@@ -60,7 +60,7 @@ const DestinationSection = () => {
         <p className='text-lg text-gray-600 '>
         Find Breathtaking Destinations, Hidden Gems, and Attractions Worth Visiting.
         </p>
-        <Link className='rounded-full text-center bg-color1 text-white p-2 text-sm hover:bg-color2 duration-300 hover:translate-x-2 flex items-center hover:shadow-lg mt-4 ' to='/destinations' target='_blank'>
+        <Link className='rounded-full text-center bg-color1 ml-1 text-white p-2 text-sm hover:bg-color2 duration-300 hover:translate-x-2 flex items-center hover:shadow-lg mt-4 ' to='/destinations' target='_blank'>
             <button>View All ⇀</button>
           </Link>
       </div>
--- frontend/src/Mainpages/HomePageSections/whatodoSection.jsx
+++ frontend/src/Mainpages/HomePageSections/whatodoSection.jsx
@@ -59,7 +59,7 @@ const whatodoSection = () => {
       </div>
 
 
-      <div className="container mx-auto px-4 pt-12">
+      <div className="container mx-auto px-4 pt-5">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
