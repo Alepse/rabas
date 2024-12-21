ee15800f8f120f9fd5ca9f6775729924dd883f91
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2024-12-22 02:23:31 +0800

    for sessions

--- backend/server.js
+++ backend/server.js
@@ -88,6 +88,7 @@ app.use(
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       secure: false, // Set to true if using HTTPS
       httpOnly: true, // Prevents client-side access to the cookie
+      sameSite: 'none', // Required for cross-origin cookies
     },
   })
 );
