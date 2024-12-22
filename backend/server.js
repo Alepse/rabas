c8884a5f00b3bc021c783c32ff496ffaed9fd927
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2024-12-22 11:07:07 +0800

    asdas

--- backend/server.js
+++ backend/server.js
@@ -86,7 +86,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       httpOnly: true,          // For security: Prevents client-side JS access to cookies
-      secure: process.env.NODE_ENV === 'production',  // Set to true when in production (HTTPS)
+      secure: false,  // Set to true when in production (HTTPS)
       sameSite: 'lax',        // Required for cross-origin cookies (ensure cookies work between frontend and backend)
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
