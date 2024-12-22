6e1a5b3f77da7b9dc7b4bfef01dcf7ccaf70b70c
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2024-12-22 09:03:44 +0800

    .sameSite: 'none', // Required for cross-origin cookiessameSite: 'none', // Required for cross-origin cookiessameSite: 'none', // Required for cross-origin cookiessameSite: 'none', // Required for cross-origin cookies

--- backend/server.js
+++ backend/server.js
@@ -87,8 +87,9 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-      secure: true, // Temporarily set to false for testing
+      secure: false, // Temporarily set to false for testing
       httpOnly: true,
+      sameSite: 'none', // Required for cross-origin cookies
     },
   })
 );
