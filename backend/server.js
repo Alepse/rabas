44b2caae06201932074b275bd4aa80b0b0800fa7
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2024-12-22 10:10:41 +0800

    https talaga

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://ubuntu-24.localhost:5173",
   "http://192.168.254.145:5173",
-  "http://rabasorsogon.com", 
   "https://rabasorsogon.com", 
-  "http://www.rabasorsogon.com",
+  "https://www.rabasorsogon.com",
   "http://147.93.19.247:5173",
 ];
 
