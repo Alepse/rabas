90ff0057830883478584713498d0548b662333d5
Alepse <kenespela@gmail.com>
2024-12-27 12:28:55 +0800

    .

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: <CustomPrevArrow />,
