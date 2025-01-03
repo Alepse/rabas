485c7471d785914197e279042b53d11d4b66ec99
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2025-01-03 18:43:07 +0800

    1

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -85,7 +85,7 @@ const Hero = () => {
         {images.map((image, index) => (
           <div key={index} className="relative h-[500px]">
             <img
-              loading='lazy'
+              loading={index === 0 ? 'eager' : 'lazy'}
               src={image}
               alt={`Slide ${index + 1}`}
               className="w-full h-full object-cover"
