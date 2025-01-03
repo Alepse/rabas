f0a769f053b9f3ffca76e1ad23701ff099e7b3ca
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2025-01-03 09:08:36 +0800

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,6 +28,6 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 3000, // Increase the chunk size warning limit to 1000 kB
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000 kB
   },
 });
