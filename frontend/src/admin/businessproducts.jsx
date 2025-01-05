42153d7f6bf19d6c852b259df2112df5d912f5cf
Jes-ton-ni <jestoniuno.vargas@bicol-u.edu.ph>
2025-01-06 04:45:12 +0800

    oks

--- frontend/src/admin/businessproducts.jsx
+++ frontend/src/admin/businessproducts.jsx
@@ -189,10 +189,10 @@ const BusinessProducts = () => {
 
           {/* Sections of business products and services */}
           <div className="w-full flex flex-wrap gap-6">
-            {<ActivitySections />}
-            { <AccommodationSection />}
-            { <RestaurantServicesSection />}
-            { <ShopSections />}
+            {showActivities && <ActivitySections />}
+            {showAccommodation && <AccommodationSection />}
+            {showRestaurantServices && <RestaurantServicesSection />}
+            {showShop && <ShopSections />}
           </div>
         </>
         )}
